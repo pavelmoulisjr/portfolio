e47d3e6d49a531de4bb720602af43468d82306e5
Pavel Moulis <pavelmoulisjr@gmail.com>
2024-08-15 14:37:47 +0200

    initial landing

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<header>
+				<h1>Pavel Moulis</h1>
+				<p>↳ Fullstack Developer </p>
+			</header>
+			<footer>
+				<ul>
+					<li>
+						<a className="mail" href="mailto:hello@pavel.dk">
+							M
+						</a>
+					</li>
+				</ul>
+			</footer>
+		</>
+	);
+}
+
+export default App;
